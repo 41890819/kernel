58be749de60c0d5e8f2f9b58f27cadf5ba7f9253
liwei <liwei@raypai.cn>
2017-03-21 16:30:30 +0800

    oled:add 300maH battery's discharge curves
    Change-Id: I548cd0532c892e879ffccd6487f6b76b52a967a2

--- include/linux/power/ricoh61x_battery_init.h
+++ include/linux/power/ricoh61x_battery_init.h
@@ -49,11 +49,11 @@ uint8_t battery_init_para[][32] = {
 #ifdef CONFIG_300MAH_RAYPAI_CAPACITY_BATTERY
 uint8_t battery_init_para[][32] = { 
         {   
-	        0x0B, 0x12, 0x0B, 0x1B, 0x0B, 0xAA, 0x0B, 0xD1,
-		0x0C, 0x06, 0x0C, 0x1B, 0x0C, 0x2E, 0x0C, 0x55,
-		0x0C, 0x92, 0x0C, 0xBF, 0x0D, 0x0A, 0x01, 0x32,
-		0x01, 0x22, 0x0F, 0xC8, 0x05, 0x2C, 0x22, 0x56 
-	}   
+		0x09, 0xD0, 0x0B, 0xAE, 0x0B, 0xD8, 0x0B, 0xFB,
+		0x0C, 0x0D, 0x0C, 0x27, 0x0C, 0x4A, 0x0C, 0x81,
+		0x0C, 0xBF, 0x0D, 0x02, 0x0D, 0x69, 0x01, 0x2C,
+		0x00, 0xA8, 0x0F, 0xC8, 0x05, 0x2C, 0x22, 0x56
+	}
 };
 #endif
 
@@ -278,3 +278,52 @@ uint8_t impe_init_para[][20] = {
    ChargeV=4.20V
    LoadMode=Load
  */
+
+/*
+   <raypai oled 300mah>
+   2017/2/10 11:36:49  Version:1.0.4
+   RTotal=401.724137931034
+   RBat=351.724137931034
+   Cavailable=291.804444444444
+   Rsense=50
+   BValue=3435
+   ChargeCompleteVoltage=4200
+   ChargeCompleteCurrent=20
+   C=300
+   I=58
+   Trest=600
+   Tdsg=1800
+   Tlast=1912
+   V0=4191.6
+   V1=4067.1
+   V2=3984.7
+   V3=3908.9
+   V4=3842.2
+   V5=3799.1
+   V6=3768.5
+   V7=3744.2
+   V8=3702.7
+   V9=3652.2
+   V10=3068.2
+   V1L=4043.2
+   V2L=3960.3
+   V3L=3884
+   V4L=3816.5
+   V5L=3772.8
+   V6L=3740.6
+   V7L=3714.9
+   V8L=3671.5
+   V9L=3617.1
+   V10L=2999.2
+   OCV_100=4191.6
+   OCV_90=4067.1
+   OCV_80=3984.7
+   OCV_70=3908.9
+   OCV_60=3842.2
+   OCV_50=3799.1
+   OCV_40=3768.5
+   OCV_30=3744.2
+   OCV_20=3702.7
+   OCV_10=3593.8
+   OCV_0=3068.2
+*/
